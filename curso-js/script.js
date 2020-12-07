e40d5361c1e0edc499f3ad66f9d6a765285e7adb
driellyps <driellys@live.com>
2020-12-07 18:46:40 +0100

    responsive faq

--- curso-js/script.js
+++ curso-js/script.js
@@ -1,4 +1,8 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
+initTabNav();
+initAccordion();
+
+function initTabNav () {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 if(tabMenu.length && tabContent.length) {
@@ -18,4 +22,22 @@ if(tabMenu.length && tabContent.length) {
       activeTab(index)
     });
   })
+}
+}
+
+function initAccordion () {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+
+  if(accordionList.length) {
+    function activeAccordion () {
+      this.classList.toggle('active')
+      this.nextElementSibling.classList.toggle('active')
+    }
+    
+    accordionList.forEach(item => {
+      item.addEventListener('click', activeAccordion)
+    })
+
+  }
+
 }
