ba9316d1db5daded5178c938ff63fa3b48b58235
driellyps <driellys@live.com>
2020-12-10 13:23:45 +0100

    format script.js

--- curso-js/script.js
+++ curso-js/script.js
@@ -3,70 +3,64 @@ initAccordion();
 softScroll();
 
 /* Nav by tabs */
-function initTabNav () {
+function initTabNav() {
   const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('active')
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('active')
 
 
-  function activeTab(index) {
-    tabContent.forEach(section => {
-      section.classList.remove('active')
-    });
-  
-    tabContent[index].classList.add('active');
+    function activeTab(index) {
+      tabContent.forEach(section => {
+        section.classList.remove('active')
+      });
+
+      tabContent[index].classList.add('active');
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        activeTab(index)
+      });
+    })
   }
-  
-  tabMenu.forEach((item, index) => {
-    item.addEventListener('click', () => {
-      activeTab(index)
-    });
-  })
-}
 }
 
 /* Accordion List */
-function initAccordion () {
+function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
 
-  if(accordionList.length) {
-    function activeAccordion () {
+  if (accordionList.length) {
+    function activeAccordion() {
       this.classList.toggle('active')
       this.nextElementSibling.classList.toggle('active')
     }
-    
+
     accordionList.forEach(item => {
       item.addEventListener('click', activeAccordion)
     })
-
   }
-
 }
 
 /* Soft Scroll on Internal Links */
 function softScroll() {
   const menuLinks = document.querySelectorAll('.js-menu a[href^="#"]');
 
-  if(menuLinks.length) {
+  if (menuLinks.length) {
     function scrollToSection(event) {
       event.preventDefault();
       const href = event.currentTarget.getAttribute('href');
       const section = document.querySelector(href);
-    
+
       section.scrollIntoView({
         behavior: 'smooth',
-        block: 'start',
+        block: 'start'
       });
     }
-    
-    
+
     menuLinks.forEach(link => {
       link.addEventListener('click', scrollToSection)
-    })
-
+    });
   }
-  
-  
 } 
